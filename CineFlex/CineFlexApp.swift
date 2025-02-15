840bd1b14634bcae9401e1d4e5a02e6366cfc32c
Sourabh Jain <GaneshShankar904@gmail.com>
2025-02-15 23:05:03 +0530

    Assignment

---
+++ CineFlex/CineFlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFlexApp.swift
+//  CineFlex
+//
+//  Created by Ganesh Shankar on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
